d89d065831419ac503c4f27ed883174938301020
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2024-12-09 20:49:59 +0300

    Made some examples for finding max array's element

---
+++ Algorithms/TypicalAlgorithms/MaxArrayElement.java
@@ -0,0 +1,44 @@
+package TypicalAlgorithms;
+
+import java.util.Arrays;
+
+
+public class MaxArrayElement {
+    /* Here we're trying to find the greatest array's element with method "sort()"
+           from Arrays
+
+    public static void main(String[] args) {
+        int[] ages = {10, 6, 15, 93, 42, 7, 32};
+        Arrays.sort(ages);
+        int maxAge = ages[ages.length - 1];
+        System.out.println(maxAge);
+        }
+*/
+
+
+            /* Here we're trying to find the greatest element from only two elements, using method
+             "max()" from Math
+
+    public static void main(String[] args) {
+        int ageMom = 45;
+        int ageDad = 47;
+
+        int maxAge = Math.max(ageDad, ageMom);
+        System.out.println(maxAge);
+        }
+*/
+
+   /* Here we're trying to find the greatest array's element using "for" cycle and method
+    "max()" from Math
+   public static void main(String[] args) {
+       int[] ages = {10, 6, 15, 93, 42, 7, 32};
+       int maxAge = 0;
+       for (int i = 0; i < ages.length; i++) {
+           maxAge = Math.max(maxAge, ages[i]);
+       }
+       System.out.println(maxAge);
+   }
+*/
+
+
+}
