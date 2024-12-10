679689fa5c69512737210d6f95c884a5497bd480
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2024-12-10 17:43:44 +0300

    Made HashFunctionsAndTables class and added some examples

---
+++ Algorithms/TypicalAlgorithms/HashFunctionsAndTables.java
@@ -0,0 +1,62 @@
+package TypicalAlgorithms;
+
+public class HashFunctionsAndTables {
+
+    /*
+    static int[] values = new int[8];
+                 Here we're going to learn something about hash-functions with examples as well
+    public static void main(String[] args) {
+                         Let's imagine that we have some students with some grades
+                         Ia : 14
+                         Anya : 99
+                         Misha : 30
+                         Anton : 42
+                         Vadim : 87
+                         Nicolai : 71
+
+        values[2] = 14;
+        values[3] = 99;
+        values[4] = 30;
+        values[5] = 42;
+        values[6] = 87;
+        values[7] = 71;
+    }
+
+                         Now let's make function that will get value from our array
+    public int getIndexByKey(String key) {
+        return key.length();
+    }
+    public int getValueByKey(String key) {
+        int index = getIndexByKey(key);
+        return values[index];
+    }
+  */
+
+
+
+
+/*
+    static int[] values = new int[40];
+    public static void main(String[] args) {
+
+                        Let's imagine that we have 40 phone numbers
+                        79101002030:900,
+                        79101234567: 100,
+                        79999999999:999,
+                        74952223344:1
+                        And we got 40 numbers in total
+
+        values[30] = 900;
+        values[7] = 100;
+        values[39] = 999;
+        values[24] = 1;
+    }
+    public int getIndexByKey(int key) {
+        return key % 40;
+    }
+    public int getValueByKey(int key) {
+        int index = getIndexByKey(key);
+        return values[index];
+    }
+ */
+}
